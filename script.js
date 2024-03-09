265e6e3a999b2f0ec7f391078c026e024ac4a46a
igorsemenovdrums <igorsemenov@gmx.com>
2024-03-09 14:58:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const C = +prompt("Введите градус Цельсия: ");
+//(C * 1.8) + 32 = F
+const F = (C * 1.8) + 32;
+console.log(F + " градусов Фаренгейта.");
+
+const x = +prompt("Градус Цельсия: ");
+const result = (x * (9/5)) + 32;
+console.log(result + " градусов Фаренгейта.");
+
+
